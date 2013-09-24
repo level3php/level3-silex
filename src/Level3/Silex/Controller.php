f12abf66183a836d8c7750728e039345521d1fa8
dpordomingo <david.pordomingo.f@gmail.com>
2013-09-24 18:44:51 +0200

    Access-Control-Allow-Origin && Access-Control-Allow-Methods

--- src/Level3/Silex/Controller.php
+++ src/Level3/Silex/Controller.php
@@ -21,6 +21,7 @@ class Controller
     private $processor;
     private $requestFactory;
     private $allowedOrigin = '';
+    private $allowedMethods = '';
 
     public function __construct(RequestProcessor $processor, RequestFactory $requestFactory)
     {
@@ -33,16 +34,28 @@ class Controller
         $this->allowedOrigin = $allowedOrigin;
     }
 
+    public function setAllowedMethods($allowedMethods)
+    {
+        $this->allowedMethods = $allowedMethods;
+    }
+
+    protected function allowOrigin(Response $response, $method){
+        $response->setHeader('Access-Control-Allow-Origin',$this->allowedOrigin);
+        $response->setHeader('Access-Control-Allow-Methods',$this->allowedMethods);
+        return $response;
+    }
+
     public function options(Request $request)
     {
         $headers = $request->headers->all();
         $headers['Access-Control-Allow-Origin'] = $this->allowedOrigin;
-
+        $headers['Access-Control-Allow-Methods'] = $this->allowedMethods;
         if (isset($headers['access-control-request-headers'])) {
             $headers['Access-Control-Allow-Headers'] = $headers['access-control-request-headers'];
         }
 
         unset($headers['access-control-request-headers']);
+        unset($headers['access-control-request-method']);
         unset($headers['user-agent']);
         unset($headers['origin']);
         unset($headers['accept']);
@@ -56,33 +69,40 @@ class Controller
     {
         $level3Request = $this->createLevel3Request($request);
         $response = $this->processor->find($level3Request);
-
-        $response->addHeader('Access-Control-Allow-Origin', '*');
+        $this->allowOrigin($response,'find');
         return $response;
     }
 
     public function get(Request $request)
     {
         $level3Request = $this->createLevel3Request($request);
-        return $this->processor->get($level3Request);
+        $response = $this->processor->get($level3Request);
+        $this->allowOrigin($response,'get');
+        return $response;
     }
 
     public function post(Request $request)
     {
         $level3Request = $this->createLevel3Request($request);
-        return $this->processor->post($level3Request);
+        $response = $this->processor->post($level3Request);
+        $this->allowOrigin($response,'post');
+        return $response;
     }
 
     public function put(Request $request)
     {
         $level3Request = $this->createLevel3Request($request);
-        return $this->processor->put($level3Request);
+        $response = $this->processor->put($level3Request);
+        $this->allowOrigin($response,'put');
+        return $response;
     }
 
     public function delete(Request $request)
     {
         $level3Request = $this->createLevel3Request($request);
-        return $this->processor->delete($level3Request);
+        $response = $this->processor->delete($level3Request);
+        $this->allowOrigin($response,'delete');
+        return $response;
     }
 
 
